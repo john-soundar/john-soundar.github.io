00e308fe54fb97f079699999088e1cf633fdb770
john-soundar <79693364+john-soundar@users.noreply.github.com>
2023-08-31 18:27:51 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/miukimiu/pen/KmzfG.
+
+ Just a full width Vertical Gallery / Content with a toggle description. 
--- js/index.js
+++ js/index.js
@@ -1,13 +1,40 @@
-$(function() {
-		var selectedClass = "";
-		$(".fil-cat").click(function(){ 
-		selectedClass = $(this).attr("data-rel"); 
-     $("#portfolio").fadeTo(100, 0.1);
-		$("#portfolio div").not("."+selectedClass).fadeOut().removeClass('scale-anm');
-    setTimeout(function() {
-      $("."+selectedClass).fadeIn().addClass('scale-anm');
-      $("#portfolio").fadeTo(300, 1);
-    }, 300); 
-		
-	});
+$(document).ready(function () {
+
+	//hide all descriptions
+	$('.description').hide();
+
+	function showFullHeight() {
+
+	   $('.gallery li').each(function() {
+
+	   	$(this).find('.btn').click(function(e){
+
+	   	console.log('Botão clicado');
+	  	
+	  	e.preventDefault();
+
+	  	 //NO MATTER WHAT WE CLOSE ALL OPEN SLIDES
+		 	$('.description').slideUp('normal');
+
+	     //IF THE NEXT SLIDE WASN'T OPEN THEN OPEN IT
+			if($(this).next().is(':hidden') == true) {
+				
+				//ADD THE ON CLASS TO THE BUTTON
+				$(this).addClass('on');
+				  
+				//OPEN THE SLIDE
+				$(this).next().slideDown('normal');
+
+
+
+			 } 
+	        
+	   }); //click
+	  });//each
+	}//function
+
+
+	//load the function when the doc is ready		
+  showFullHeight();
+  
 });
